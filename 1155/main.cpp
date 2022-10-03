1466acb244e57cafddcb4698f58b29c0d1303840
lmmqxyx <1368470804@qq.com>
2022-10-03 23:49:36 +0800

    solve the problem 1155

--- 1155/main.cpp
+++ 1155/main.cpp
@@ -5,31 +5,30 @@ class Solution
 {
 public:
   vector<int> vv;
-  int nn, kk, target, ans;
+  // int nn, kk, target, ans;
+  const int mod_num = 1e9 + 7;
+  int dp[32][1010];
   int solve(int n, int k, int target)
   {
-    if (target < 0)
+    if (target < 1)
       return 0;
-
-    if ((n == 0 && target != 0) || (n != 0 && target == 0))
+    if (n == 1)
+    {
+      return target <= k ? 1 : 0;
+    }
+    long long total = (long long)n * (long long)k;
+    if (total < target)
       return 0;
-
-    if (n == 0 && target == 0)
-      return 1;
-
     int ans = 0;
     for (int i = 1; i <= k; i++)
     {
-      ans = ans + solve(n - 1, k, target - i);
+      dp[n - 1][target - i] = dp[n - 1][target - i] == 0 ? solve(n - 1, k, target - i) : dp[n - 1][target - i];
+      ans = (ans + dp[n - 1][target - i]) % mod_num;
     }
-
-    int mod = 1e+7;
-    return ans % mod;
+    return ans;
   }
-
   int numRollsToTarget(int n, int k, int target)
   {
-
     return solve(n, k, target);
   }
 };
@@ -42,7 +41,8 @@ int main()
   //  ios::sync_with_stdio(false);
   auto ans = new Solution();
   // cout << ans->numRollsToTarget(1, 6, 3) << endl;
-  //  cout << ans->numRollsToTarget(2, 6, 7) << endl;
-  cout << ans->numRollsToTarget(10, 3, 20) << endl;
+  // cout << ans->numRollsToTarget(2, 6, 7) << endl;
+  cout << ans->numRollsToTarget(30, 30, 500) << endl;
+  // cout << ans->numRollsToTarget(10, 3, 20) << endl;
   return 0;
 }
